54bedcda29c041aff6b6244168f111f20a21e29d
Zane Willgruber <73185743+ZaneWillgruber@users.noreply.github.com>
2025-06-11 18:21:20 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PACKAGES=(
+	"spotify-launcher"
+	"ttf-nerd-fonts-symbols"
+	"neofetch"
+)
+
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+
+if ! command -v yay &> /dev/null; then
+	echo "yay is not installed. Installing yay..."
+	sudo pacman -S --needed base-devel git
+	cd ~
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+else
+	echo "yay is already installed."
+fi
+
+yay -Syu --noconfirm
+
+for package in "${PACKAGES[@]}"; do
+	if yay -Qi "$package" &> /dev/null; then
+		echo "$package is already installed. Skipping..."
+	else
+		echo "$package is not installed. Installing..."
+		yay -S "$package" --noconfirm
+	fi
+done
